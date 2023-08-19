a3104a42177fd238f50e31609a9415c1c0531aa8
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-08-19 15:39:41 -0400

    components+pages/index

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import {Link} from 'react-router-dom';
+
+import {Link} from 'react-router-dom';
+
+function Header() {
+    return (
+        <nav className="nav">
+            <Link to="/">
+                <div>Places App</div>
+            </Link>
+        </nav>
+    )
+}
+
+export default Header;
