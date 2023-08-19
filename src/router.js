b2506344216ea76654dc8d7abc9e89ec0c4d3697
Arielle Gironza <ariellegironza@Arielles-Air.fios-router.home>
2023-08-19 16:46:21 -0400

    updateAction and router.js updated

--- src/router.js
+++ src/router.js
@@ -8,16 +8,16 @@ import Show from "./Pages/Show";
 // import Signup from "./Pages/Signup";
 // import Login from "./Pages/Login";
 import { placesLoader, placeLoader } from "./loaders";
-import { createAction } from "./actions";
-// import { updateAction, deleteAction, signupAction, loginAction } from "./actions"
+import { createAction, updateAction } from "./actions";
+// import { deleteAction, signupAction, loginAction } from "./actions"
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path="/" element={<App/>}>
         <Route path="" element={<Index/>} loader={placesLoader}/>
         <Route path=":id" element={<Show/>} loader={placeLoader}/>
         <Route path="create" action={createAction}/>
-        {/* <Route path="update/:id" action={updateAction}/>
-        <Route delete="delete/:id" action={deleteAction}/> */}
+        <Route path="update/:id" action={updateAction}/>
+        {/* <Route delete="delete/:id" action={deleteAction}/> */}
     </Route>
 ));
 
