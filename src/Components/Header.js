ad1b2d36a8a047c4743e326ad378fbd8bd488186
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-08-23 18:54:14 -0400

    header logo fix

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,3 +1,4 @@
+import logo from '../Images/logo.png';
 import {Link} from 'react-router-dom';
 
 function Header() {
@@ -5,11 +6,12 @@ function Header() {
         <div className='header'>
             <div className='logo'>
         <Link to="/">
-            <img src="../Images/logo.png" alt="BonVoyage"/>
+            <img src={logo} alt="BonVoyage"/>
         </Link>
     </div>
             <div className='navbar'>
                 <ul>
+                    {/* HERE IS WHERE DROPDOWN MENU CODE SHOULD GO */}
                     <Link to="/"><li>Add New</li></Link>
                     <Link><li>About</li></Link>
                     <li><a href="#">Ideas</a></li>
