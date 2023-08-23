418a558c8eb2e721c0fbf83ea805f8118db4dd75
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-08-22 21:04:30 -0400

    Signup.js and Login.js cleanup

--- src/Pages/Signup.js
+++ src/Pages/Signup.js
@@ -1,13 +1,13 @@
 import { Form } from "react-router-dom";
 
 function Signup(props){
-    return <div>
-        <h3>Signup Form</h3>
+    return <div className="signup-form">
+        <h2>Signup</h2>
         <Form action="/signup" method="POST">
             <label htmlFor="username">Username</label>
             <input type="text" name="username" id="username" />
             <label htmlFor="password">Password</label>
-            <input type="password" name="password" id="password" />
+            <input type="password" name="password" id="password" /><br/>
             <input type="submit" value="Signup" />
         </Form>
     </div>
