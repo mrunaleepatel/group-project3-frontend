b74e519412aa9abbfe844f5603afaed2a614ec42
Mrunalee Patel <mrunaleepatel997@gmail.com>
2023-08-23 19:03:30 -0500

    more style

--- src/Pages/Main.js
+++ src/Pages/Main.js
@@ -3,12 +3,13 @@ import {Outlet, Link} from 'react-router-dom';
 function Main(props){
     return <div>
         <h1>Welcome to the Places App</h1>
+        <div className="buttons">
+        <Link to="/signup"><button><h3>Signup</h3></button></Link>
         
-        <Link to="/signup"><button>Signup</button></Link>
-        
-        <Link to="/login"><button>Login</button></Link>
+        <Link to="/login"><button><h3>Login</h3></button></Link>
 
         <Outlet />
+        </div>
     </div>
 }
 
