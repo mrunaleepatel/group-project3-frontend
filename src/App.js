8d4d1f948f5e6861396ee8ed0046a429935a56e5
Arielle Gironza <ariellegironza@Arielles-MacBook-Air.local>
2023-08-26 09:29:34 -0400

    playing with dropdown outline

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Header from "./Components/Header"
+import Dropdown from "./Components/Dropdown";
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <Dropdown/>
       <Outlet/>
     </div>
   );
--- src/Components/Dropdown.js
+++ src/Components/Dropdown.js
@@ -1,29 +1,60 @@
 import React from "react";
 import '../styles.scss'
+import './dropdown.css'
 
-const Dropdown = () => {
+const Apps = () => {
     const [open, setOpen] = React.useState(false);
 
     const handleOpen = () => {
         setOpen(!open);
     };
 
+    const handleItemOne = () => {
+        setOpen(false);
+    };
+
+    const handleItemTwo = () => {
+        setOpen(false);
+    };
+
     return (
-        <div className="dropdown">
-            <button onClick={handleOpen}>Add New</button>
+        <Dropdown
+        open={open}
+        trigger={<button onClick={handleOpen}>Add New</button>}
+        menu={[
+            <button onClick={handleItemOne}>Item 1</button>,
+            <button onClick={handleItemTwo}>Item 2</button>,
+        ]}
+        />
+    );
+};
+
+const Dropdown = ({ open, trigger, menu }) => {
+    return (
+        <div className="dropdownForm">
+            {trigger}
             {open ? (
-            <ul className="test-menu">
-                <li className="test-item">
-                    <button>Item 1</button>
-                </li>
-                <li className="test-item">
-                    <button>Item 2</button>
-                </li>
-            </ul>
+                <ul className="testMenu">
+                    {menu.map((menuItem, index) => (
+                        <li key={index} className="menuItem">{menuItem}</li>
+                    ))})
+                </ul>
             ) : null}
-            {open ? <div>Is Open</div> : <div>Is Closed</div>}
         </div>
     );
 };
+
+            // {open ? (
+            // <ul className="test-menu">
+            //     <li className="test-item">
+            //         <button onClick={handleItemOne}>Item 1</button>
+            //     </li>
+            //     <li className="test-item">
+            //         <button onClick={handleItemTwo}>Item 2</button>
+            //     </li>
+            // </ul>
+            // ) : null}
+            {/* {open ? <div>Is Open</div> : <div>Is Closed</div>} */}
+
 
 export default Dropdown;
